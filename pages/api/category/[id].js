dc68cf35943a799f8f49237d1e0b87f6969281ef
rahimlisarkhan <whelp@Whelps-MacBook-Air.local>
2023-09-27 16:03:02 +0400

    updatE

--- pages/api/category/[id].js
+++ pages/api/category/[id].js
@@ -5,7 +5,7 @@ import {
   handlerCategoryDELETE,
   handlerCategoryGETID,
   handlerCategoryPUT,
-} from "routes/category";
+} from "../../../server/routes/category";
 
 export default async function handler(req, res) {
   // Set CORS headers to allow requests from any origin
