16017d821c502369f4b09276132152ec04c9f59e
rahimlisarkhan <whelp@Whelps-MacBook-Air.local>
2023-09-27 16:26:11 +0400

    update

--- server/configs/firebase.js
+++ server/configs/firebase.js
@@ -3,14 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAVP4NxAKD8wH765Qg7O5RuwpT3AgfI_MA",
-  authDomain: "foodyapp-51b6e.firebaseapp.com",
-  projectId: "foodyapp-51b6e",
-  storageBucket: "foodyapp-51b6e.appspot.com",
-  messagingSenderId: "933394810155",
-  appId: "1:933394810155:web:d59b1137c370f3af72716e",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
