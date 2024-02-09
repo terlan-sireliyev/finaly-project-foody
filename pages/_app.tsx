9e24cfb9bafeedc968f5cd8bd638989b3f437949
TerlaN2233 <95983928+TerlaN2233@users.noreply.github.com>
2024-02-09 12:32:20 +0300

    add changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import { useRouter } from "next/router";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import AdminLayout from "../components/adminLayout";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  if (router.pathname == "/admin/login")
+    return (
+      <>
+        <Component {...pageProps} />
+      </>
+    );
+  else if (router.pathname.includes("/admin")) {
+    return (
+      <>
+        <AdminLayout>
+          <Component {...pageProps} />
+        </AdminLayout>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <>
+          <Component {...pageProps} />
+        </>
+      </>
+    );
+  }
 }
 
-export default MyApp
+export default MyApp;
